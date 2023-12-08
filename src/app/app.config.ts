c838ac9eb4417051fefc2aa47f60e1b1783af679
asmaebouch <asmae.bouchekara@gmail.com>
2023-12-08 18:16:25 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, EnvironmentProviders, NgModule} from '@angular/core';
+import {provideRouter, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
 
 import { routes } from './app.routes';
+import {HeaderComponent} from "./header/header.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
-export const appConfig: ApplicationConfig = {
+export const appConfig: { imports: any[]; declarations: any[]; providers: EnvironmentProviders[] } = {
+
+    declarations: [
+      AppComponent,
+
+      HeaderComponent,
+
+    ],
+    imports: [
+      BrowserModule,
+      ReactiveFormsModule, // Add ReactiveFormsModule here
+      FormsModule,
+      HttpClientModule,
+      RouterModule,
+
+
+    ],
   providers: [provideRouter(routes)]
 };
